0733d866f934524272d4a2fac17e554eee3740cc
LN <LN>
2014-08-26 14:54:06 -0700

    extracting all the jar libraries into an external Android library project

--- src/edu/cmu/sv/mobisens/client/example/MobiSensClientExampleApplication.java
+++ src/edu/cmu/sv/mobisens/client/example/MobiSensClientExampleApplication.java
@@ -1,18 +1,7 @@
 package edu.cmu.sv.mobisens.client.example;
 
-import org.acra.ACRA;
-import org.acra.ErrorReporter;
-import org.acra.annotation.ReportsCrashes;
-
 import android.app.Application;
-import android.content.Context;
-import android.content.SharedPreferences;
-
-import com.crittercism.app.Crittercism;
-
-import edu.cmu.sv.mobisens.library.managers.MSManager;
 import edu.cmu.sv.mobisens.library.managers.MobiSensLibraryApplication;
-import edu.cmu.sv.mobisens.library.tools.DeviceManager;
 
 
 public class MobiSensClientExampleApplication extends Application {
