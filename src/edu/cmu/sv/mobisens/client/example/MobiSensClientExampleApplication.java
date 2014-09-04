4d9bb08037f80aee6e3a4f8edcf36775862c3c76
LN <LN>
2014-09-03 17:20:22 -0700

    updated library

--- src/edu/cmu/sv/mobisens/client/example/MobiSensClientExampleApplication.java
+++ src/edu/cmu/sv/mobisens/client/example/MobiSensClientExampleApplication.java
@@ -1,7 +1,17 @@
 package edu.cmu.sv.mobisens.client.example;
-
 import android.app.Application;
+
 import edu.cmu.sv.mobisens.library.api.ContextManager;
+
+
+import org.acra.annotation.*;
+@ReportsCrashes(
+    formKey="",
+    formUri = "https://mobisens.cloudant.com/acra-mobisens/_design/acra-storage/_update/report",
+    reportType = org.acra.sender.HttpSender.Type.JSON,
+    httpMethod = org.acra.sender.HttpSender.Method.PUT
+)
+
 
 
 public class MobiSensClientExampleApplication extends Application {
